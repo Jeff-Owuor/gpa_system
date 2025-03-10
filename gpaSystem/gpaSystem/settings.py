32e31c0ba7407835b36b0a09ca1bc662fc1a841d
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2025-03-10 13:53:51 +0300

    Established connection to postgres database

--- gpaSystem/gpaSystem/settings.py
+++ gpaSystem/gpaSystem/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'gpaSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apt2080',
+        'USER': 'paulineapondi',
+        'PASSWORD': '1989',
+        'HOST': '127.0.0.1',  # or use the server IP
+        'PORT': '5432',
     }
 }
 
