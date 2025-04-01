166fdd95d4b09a13a1f30a3fea4b08fa7995768b
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2025-04-01 16:02:50 +0300

    add all endpoints

--- gpaSystem/urls.py
+++ gpaSystem/urls.py
@@ -4,6 +4,8 @@ from django.urls import path,include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework.routers import DefaultRouter
+from .views import CustomUserViewSet, StudentViewSet, CourseViewSet, EnrollmentViewSet, AssessmentViewSet, ScoreViewSet, GradeScaleViewSet, ProjectionViewSet
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -14,6 +16,17 @@ schema_view = get_schema_view(
     public = True,
     permission_classes = (permissions.AllowAny,),
 )
+
+router = DefaultRouter()
+router.register(r'users', CustomUserViewSet)
+router.register(r'students', StudentViewSet)
+router.register(r'courses', CourseViewSet)
+router.register(r'enrollments', EnrollmentViewSet)
+router.register(r'assessments', AssessmentViewSet)
+router.register(r'scores', ScoreViewSet)
+router.register(r'grade-scales', GradeScaleViewSet)
+router.register(r'projections', ProjectionViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +41,7 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/",schema_view.with_ui("redoc",cache_timeout=0),name="schema-redoc"),
+    path('api/', include(router.urls)),
+    
     
 ]
