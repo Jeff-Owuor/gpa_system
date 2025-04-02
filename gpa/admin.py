3b56757f1c85684a34178e150e0231d9644b501e
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2025-04-02 08:29:08 +0300

    fixed database problems

--- gpa/admin.py
+++ gpa/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUser, Student, Course, Enrollment, Assessment, Score, GradeScale, Projection
 
-# Register your models here.
+# Register your models
+admin.site.register(CustomUser)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Enrollment)
+admin.site.register(Assessment)
+admin.site.register(Score)
+admin.site.register(GradeScale)
+admin.site.register(Projection)
