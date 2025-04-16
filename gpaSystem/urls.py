69db1a8d350f59c284555ec1a47e0d0dd095dbe2
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2025-04-16 12:37:08 +0300

    Add assessment type choices temporary view

--- gpaSystem/urls.py
+++ gpaSystem/urls.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.routers import DefaultRouter
-from gpa.views import StudentViewSet, CourseViewSet, EnrollmentViewSet, AssessmentViewSet, ScoreViewSet, GradeScaleViewSet, ProjectionViewSet, MajorListView
+from gpa.views import StudentViewSet, CourseViewSet, EnrollmentViewSet, AssessmentViewSet, ScoreViewSet, GradeScaleViewSet, ProjectionViewSet, MajorListView,AssessmentTypeChoices
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -41,6 +41,7 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/",schema_view.with_ui("redoc",cache_timeout=0),name="schema-redoc"),
+    path('assessment-types/', AssessmentTypeChoices.as_view(), name='assessment-types'),
     path('api/', include(router.urls)),
     
     
